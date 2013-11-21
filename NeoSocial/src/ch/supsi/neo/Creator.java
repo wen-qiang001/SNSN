0745342fa62ba4cd226ef776c8d4eb7385ffd381
galliva <galliva@gmail.com>
2013-11-21 16:47:45 +0100

    different approach

--- NeoSocial/src/ch/supsi/neo/Creator.java
+++ NeoSocial/src/ch/supsi/neo/Creator.java
@@ -3,95 +3,69 @@ package ch.supsi.neo;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Random;
 
-import org.neo4j.graphdb.GraphDatabaseService;
+import org.neo4j.graphdb.DynamicLabel;
+import org.neo4j.graphdb.DynamicRelationshipType;
 import org.neo4j.graphdb.Label;
-import org.neo4j.graphdb.Node;
-import org.neo4j.graphdb.Transaction;
-import org.neo4j.graphdb.factory.GraphDatabaseFactory;
-import org.neo4j.graphdb.factory.GraphDatabaseSettings;
-import org.neo4j.graphdb.index.Index;
+import org.neo4j.graphdb.RelationshipType;
+import org.neo4j.helpers.collection.MapUtil;
+import org.neo4j.index.lucene.unsafe.batchinsert.LuceneBatchInserterIndexProvider;
+import org.neo4j.unsafe.batchinsert.BatchInserter;
+import org.neo4j.unsafe.batchinsert.BatchInserterIndex;
+import org.neo4j.unsafe.batchinsert.BatchInserterIndexProvider;
+import org.neo4j.unsafe.batchinsert.BatchInserters;
 
 public class Creator 
 {	
-	private GraphDatabaseService db;
-	private Node switzerland;
-	
-	private Transaction tx;
-	private Index<Node> municipalitiesIndex;	
-	private Index<Node> inhabitantsIndex;	
-	
-	private Random rand = new Random();
-	private String[][] names = Utilities.getNames();
-	
+	private BatchInserter inserter;
+	private BatchInserterIndexProvider indexProvider;
+	private BatchInserterIndex geoIndex;
+	private BatchInserterIndex peopleIndex;
+
+	private Label cantonLabel = DynamicLabel.label("Canton");
+	private Label regionLabel = DynamicLabel.label("Region");
+	private Label municiLabel = DynamicLabel.label("Municipality");
+	private Label personLabel = DynamicLabel.label("Person");
+	private Label maleLabel   = DynamicLabel.label("Male");
+	private Label femaleLabel = DynamicLabel.label("Female");
+
+	private RelationshipType isIn     = DynamicRelationshipType.withName("IS_IN");
+	private RelationshipType livesIn  = DynamicRelationshipType.withName("LIVES_IN");
+	private RelationshipType friendOf = DynamicRelationshipType.withName("FRIEND_OF");
+
+	private long switzerland;	
 	private int cantonsAdded = 0;
 	private int regionsAdded = 0;
-	private HashMap<String, Integer> municipalities = new HashMap<String, Integer>();
+	private HashMap<Long, Integer> municipalities = new HashMap<Long, Integer>();
 	private int inhabitantsAdded = 0;
-	private List<Integer> socialInhabitants = new ArrayList<Integer>();
+	private List<Long> socialInhabitants = new ArrayList<Long>();
+
+	private Random rand = new Random();
+	private String[][] names = Utilities.getNames();
 	private int friendsAdded = 0;
-	
+
 	private long lStartTime;
 	private long lEndTime;
-	
-	/**
-	 * Open or creates the DB.
-	 */
-	@SuppressWarnings("deprecation")
-	public void openOrCreateDB()
-	{
-		db = new GraphDatabaseFactory().
-				newEmbeddedDatabaseBuilder(Main.dbPath)
-				.setConfig(GraphDatabaseSettings.node_keys_indexable, "name")
-				//.setConfig(GraphDatabaseSettings.relationship_keys_indexable, "IS_IN, LIVES_IN, FRIEND_OF")
-				.setConfig(GraphDatabaseSettings.node_auto_indexing, "true")
-				//.setConfig(GraphDatabaseSettings.relationship_auto_indexing, "true")
-				.newGraphDatabase();		
 
-		registerShutdownHook(db);		
-		
-		tx = db.beginTx();
-		municipalitiesIndex = db.index().forNodes(Main.municipalitiesIndex);
-		inhabitantsIndex    = db.index().forNodes(Main.inhabitantsIndex);
-		
-		tx.success();
-		tx.finish();
-	}
 	
-	/**
-	 * Closes the DB.
-	 */
-	public void closeDB()
-	{
-		db.shutdown();	
-		System.gc();
-	}
+	
 	
 	/**
 	 * Adds cantons, regions and municipalities.
 	 */
 	public void addGeographicalInfo()
-	{	
-		tx = db.beginTx();
+	{							
+		inserter = BatchInserters.inserter(Main.dbPath);
+		indexProvider = new LuceneBatchInserterIndexProvider(inserter);
 		
-		try
-		{						
-			addSwitzerland();
-			addGeo();
-			
-			tx.success();
-		}
-		catch (Exception e)
-		{
-			System.out.println(e.toString());
-			tx.failure();
-		}
-		finally
-		{
-			tx.close();
-		}
+		geoIndex = indexProvider.nodeIndex("geoIndex", MapUtil.stringMap("type", "exact"));
+		geoIndex.setCacheCapacity("name", 3000);
+		
+		addSwitzerland();
+		addGeo();
 	}
 
 	/**
@@ -99,8 +73,9 @@ public class Creator
 	 */
 	private void addSwitzerland()
 	{
-		switzerland = db.createNode();
-		switzerland.setProperty("name", "Switzerland");
+		Map<String, Object> properties = new HashMap<>();	
+		properties.put("name", "Switzerland");
+		switzerland = inserter.createNode(properties);
 	}
 
 	/**
@@ -109,210 +84,162 @@ public class Creator
 	private void addGeo()
 	{
 		System.out.println("Adding geographical information...");
-		
+
 		lStartTime = System.currentTimeMillis();
-		
+
 		List<String[]> m = Utilities.splitFile(Main.filesPath + "geo.csv");
-		
-		Node currentCanton = null;
-		Node currentRegion = null;
-		
+
+		long currentCanton = -1;
+		long currentRegion = -1;
+
 		for (String[] s : m) 
 		{			
 			if(s[0].startsWith("- "))
 			{
-				Node n = db.createNode();
-				
-				n.addLabel(new Label() {
-					@Override
-					public String name() {
-						return "Canton";
-					}
-				});
-				
-				n.setProperty("name",  s[0].substring(2));
+				Map<String, Object> properties = new HashMap<>();
+				properties.put("name", s[0].substring(2));
 
-				n.createRelationshipTo(switzerland, Relation.RelTypes.IS_IN);
+				currentCanton = inserter.createNode(properties, cantonLabel);
 
-				currentCanton = n;
-				
+				inserter.createRelationship(currentCanton, switzerland, isIn, null );
+
+				geoIndex.add(currentCanton, properties);
+
 				cantonsAdded++;
 			}
 			if(s[0].startsWith(">> "))
 			{
-				Node n = db.createNode();
-				
-				n.addLabel(new Label() {
-					@Override
-					public String name() {
-						return "Region";
-					}
-				});
-				
-				n.setProperty("name" , s[0].substring(3));
+				Map<String, Object> properties = new HashMap<>();
+				properties.put("name", s[0].substring(3));
 
-				if(currentCanton != null)
-					n.createRelationshipTo(currentCanton, Relation.RelTypes.IS_IN);
+				currentRegion = inserter.createNode(properties, regionLabel);
 
-				currentRegion = n;
-				
+				if(currentCanton != -1)
+					inserter.createRelationship(currentRegion, currentCanton, isIn, null );
+
+				geoIndex.add(currentRegion, properties);
+
 				regionsAdded++;
 			}
 			if(s[0].startsWith("......"))
 			{
-				String id   = s[0].substring(6, 10);
 				String name = s[0].substring(11);
 				int people  = Integer.valueOf(s[1].trim().replace(" ", ""));
-				
-				municipalities.put(id, people);
-								
-				Node n = db.createNode();
-				
-				n.addLabel(new Label() {
-					@Override
-					public String name() {
-						return "Municipality";
-					}
-				});
-				
-				n.setProperty("name",  name);
 
-				municipalitiesIndex.add(n, "id", id);
-				
-				if(currentRegion != null)
-					n.createRelationshipTo(currentRegion, Relation.RelTypes.IS_IN);
-				
-				if(municipalities.size() >= Main.maxMunicipalities)
-					break;
+				Map<String, Object> properties = new HashMap<>();
+				properties.put("name", name);
+
+				long currentMunicipality = inserter.createNode(properties, municiLabel);
+
+				municipalities.put(currentMunicipality, people);
+
+				if(currentRegion != -1)
+					inserter.createRelationship(currentMunicipality, currentRegion, isIn, null );
+
+				geoIndex.add(currentMunicipality, properties);
 			}
 		}
-		
+
+		indexProvider.shutdown();
+		inserter.shutdown();
+
 		lEndTime = System.currentTimeMillis();
-		
+
 		System.out.println(String.format("Added %d cantons,  %d regions, %d municipalities in %.2f seconds", cantonsAdded, regionsAdded, municipalities.size(), (lEndTime - lStartTime)/1000.0));
 	}
 
+	
+	
+	
 	/**
-	 * Adds some inhabitants to the municipalities.
+	 * Adds some inhabitants to the graph.
 	 * @param node
 	 * @param no
 	 */
 	public void addInhabitants()
 	{
 		System.out.println("Adding inhabitants...");
-		
+
 		lStartTime = System.currentTimeMillis();
 		
-		inhabitantsAdded = 0;
-				
-		int i = 0;
+		inserter = BatchInserters.inserter(Main.dbPath);
+		indexProvider = new LuceneBatchInserterIndexProvider(inserter);
 		
-		for (Entry<String, Integer> entry : municipalities.entrySet())
+		peopleIndex = indexProvider.nodeIndex("peopleIndex", MapUtil.stringMap("type", "exact"));
+		peopleIndex.setCacheCapacity("first_name", 8000000);
+		peopleIndex.setCacheCapacity("last_name", 8000000);
+
+		inhabitantsAdded = 0;
+
+		int i = 0;
+
+		for (Entry<Long, Integer> entry : municipalities.entrySet())
 		{
 			if(i % 10 == 0)
 				System.out.println(String.format("%d / %d - %.2f %% -  %d / %d municipalities", inhabitantsAdded, Main.totalInhabitants, ((float)inhabitantsAdded/(float)Main.totalInhabitants)*100f, i, municipalities.size()));
-			
+
 			addInhabitantsInMunicipality(entry.getKey(), entry.getValue());
-									
+
 			i++;
 		}
-		
+
+		indexProvider.shutdown();
+		inserter.shutdown();
+
 		lEndTime = System.currentTimeMillis();
-		
+
 		System.out.println(String.format("Added %d inhabitants (social %d) in %.2f seconds", inhabitantsAdded, socialInhabitants.size(), (lEndTime - lStartTime) / 1000.0));
 	}
-	
-	private void addInhabitantsInMunicipality(String municipality, int no)
-	{						
-		try
-		{		
-			tx = db.beginTx();
-			Node found = municipalitiesIndex.get("id", municipality).getSingle();
-			tx.success();
-			tx.close();
-			
-			tx = db.beginTx();
-			
-			if(found != null)
-			{				
-				for (int i = 0; i < no; i++)
-				{					
-					Node person = db.createNode();
-					
-					person.addLabel(new Label() {
-						@Override
-						public String name() {
-							return "Person";
-						}
-					});
-				
-					String personId = String.format("p%d", ++inhabitantsAdded);
-					
-					// male of female (http://www.bfs.admin.ch/bfs/portal/en/index/themen/01/02/blank/key/alter/nach_geschlecht.html)
-					int maleOrFemale = rand.nextInt(1000);
-					
-					String name = "";
 
-					if(maleOrFemale < 494)
-					{
-						int rm = rand.nextInt(names[1].length);
-						name = names[1][rm];
+	/**
+	 * Adds the inhabitants of a particular municipality.
+	 * @param municipality
+	 * @param no
+	 */
+	private void addInhabitantsInMunicipality(long municipality, int no)
+	{                                                
+		for (int i = 0; i < no; i++)
+		{        
+			// male of female (http://www.bfs.admin.ch/bfs/portal/en/index/themen/01/02/blank/key/alter/nach_geschlecht.html)
+			int maleOrFemale = rand.nextInt(1000);
 
-						person.addLabel(new Label() {
-							@Override
-							public String name() {
-								return "Male";
-							}
-						});
-					}
-					else
-					{
-						int rf = rand.nextInt(names[0].length);
-						name = names[0][rf];
-						
-						person.addLabel(new Label() {
-							@Override
-							public String name() {
-								return "Female";
-							}
-						});
-					}
+			String firstName = "";
+			String lastName  = "";
+			boolean male;
 
-					int rl = rand.nextInt(names[2].length);
-					
-					name = name + " " + names[2][rl];
-					
-					person.setProperty("name",  name);
-					
-					// social (http://en.wikipedia.org/wiki/Facebook_statistics)
-					int social = rand.nextInt(1000);
-					
-					if(social < 385)
-						socialInhabitants.add(inhabitantsAdded);
-						
-					inhabitantsIndex.add(person, "id", personId);
-					
-					person.createRelationshipTo(found, Relation.RelTypes.LIVES_IN);
-					
-					if(i > 0 && i % Main.transactionsSize == 0)
-					{
-						tx.success();
-						tx.close();
-						tx = db.beginTx();
-					}
-				}	
-				
-				found = null;
+			if(maleOrFemale < 494)
+			{
+				int rm = rand.nextInt(names[1].length);
+				firstName = names[1][rm];
+				male = true;
 			}
+			else
+			{
+				int rf = rand.nextInt(names[0].length);
+				firstName = names[0][rf];
+				male = false;    
+			}
+
+			int rl = rand.nextInt(names[2].length);
+			lastName = names[2][rl];
+
+			Map<String, Object> properties = new HashMap<>();
+			properties.put("first_name", firstName);
+			properties.put("last_name",  lastName);
+
+			long inhabitant = inserter.createNode(properties, personLabel, (male) ? maleLabel : femaleLabel);
+
+			inserter.createRelationship(inhabitant, municipality, livesIn, null );
 			
-			tx.success();
-			tx.close();
-		}
-		catch (Exception e)
-		{
-			e.printStackTrace();
-			System.out.println(e.toString());
-			tx.failure();
+			peopleIndex.add(inhabitant, properties);
+
+			// social (http://en.wikipedia.org/wiki/Facebook_statistics)
+			int social = rand.nextInt(1000);
+
+			if(social < 385)
+				socialInhabitants.add(inhabitant);
+
+			inhabitantsAdded++;
 		}
 	}
 
@@ -320,101 +247,64 @@ public class Creator
 	 * Adds some friends to the inhabitants.
 	 */
 	public void addFriends()
-	{		
+	{                
 		System.out.println("Adding friends...");
-		
+
 		lStartTime = System.currentTimeMillis();
-				
+
 		friendsAdded = 0;
-		
+
 		int added = 0;
-		
-		for (int inhabitant : socialInhabitants) 
+
+		for (long inhabitant : socialInhabitants) 
 		{
 			//  the average friend count is 90 
 			double n = rand.nextGaussian()*9 + 90;
-			
+
 			addFriendsToInhabitant(inhabitant, (int)n);
-			
+
 			if(added % 500 == 0)
 				System.out.println(String.format("%d / %d inhabitants - %.2f %%", added, socialInhabitants.size(), ((float)added/(float)socialInhabitants.size())*100f));
-			
+
 			added++;
 		}
-					
+
 		lEndTime = System.currentTimeMillis();
-		
+
 		System.out.println(String.format("Added %d friends in %.2f seconds - avg. friends %.2f", friendsAdded, (lEndTime - lStartTime) / 1000.0, (float)friendsAdded/(float)socialInhabitants.size()));
 	}
-	
-	private void addFriendsToInhabitant(int inhabitant, int no)
-	{		
-		try
-		{					
-			String personId = String.format("p%d", inhabitant);
-			
-			tx = db.beginTx();
-			
-			Node found = inhabitantsIndex.get("id", personId).getSingle();
-						
-			if(found != null)
+
+	private void addFriendsToInhabitant(long inhabitant, int no)
+	{                
+		for (int i = 0; i < no; i++)
+		{                                        
+			int r;
+
+			while(true)
 			{
-				Node friend;
-				
-				for (int i = 0; i < no; i++)
-				{					
-					int r;
-					
-					while(true)
-					{
-						r = rand.nextInt(inhabitantsAdded) + 1;
-						
-						if(r!=inhabitant)
-							break;
-					}
-										
-					friend = inhabitantsIndex.get("id", "p" + r).next();
-					
-					if(friend != null)
-					{
-						found.createRelationshipTo(friend, Relation.RelTypes.FRIEND_OF);
-						friendsAdded++;
-					}
-				}
+				r = rand.nextInt(inhabitantsAdded);
+
+				if(r!=inhabitant)
+					break;
 			}
 			
-			tx.success();
-		}
-		catch (Exception e)
-		{
-			System.out.println(e.toString());
-			tx.failure();
-		}
-		finally
-		{
-			tx.close();
+			long friend = socialInhabitants.get(r);
+
+			inserter.createRelationship(inhabitant, friend, friendOf, null );
+
+			friendsAdded++;
 		}
 	}
-	
+
 	public void printStats()
 	{
 		String s = "";
 		s += String.format("TOT. nodes: %d\n", 1+1+cantonsAdded+regionsAdded+municipalities.size()+inhabitantsAdded);
 		s += String.format("social inhabitants: %d (%.2f %%)\n", socialInhabitants.size(), ((float)socialInhabitants.size()/(float)inhabitantsAdded)*100f);
 		s += String.format("TOT. relations: %d", friendsAdded);
-		
+
 		System.out.println(s);
 	}
-	
-	private static void registerShutdownHook(final GraphDatabaseService graphDb)
-	{
-		Runtime.getRuntime().addShutdownHook(new Thread()
-		{
-			@Override
-			public void run()
-			{
-				graphDb.shutdown();
-			}
-		});
-	}
 }
+
+
--- NeoSocial/src/ch/supsi/neo/Main.java
+++ NeoSocial/src/ch/supsi/neo/Main.java
@@ -5,26 +5,24 @@ import java.io.File;
 import org.apache.commons.io.FileUtils;
 
 public class Main 
-{
+{	
+	public static int totalInhabitants  = 8039060;
+	
+//	public static boolean recreateFiles  = false;
+//	public static String outputPath      = "/Users/galliva/Desktop/social/output/";
+//	public static String outputSeparator = ";"; 
+//	public static String outputGFile     = "geo.csv";
+//	public static String outputIFile     = "inhabitants.csv";
+
 	public static boolean startStopService = true;
 	public static boolean removePrevious   = true;
-	
 	public static String neoBin    = "/Users/galliva/Desktop/social/neo4j-community-2.0.0-RC1/bin/neo4j";
 	public static String dbPath    = "/Users/galliva/Desktop/social/social_graph.db";
 	public static String filesPath = "/Users/galliva/Desktop/social/data/";
-	//public static String dbPath    = "c:\\Users\\Install\\Desktop\\social_graph.db";
-	//public static String filesPath = "c:\\Users\\Install\\Desktop\\data\\";
 	
 	public static String maleNamesFile   = "swiss_names_M.csv";
 	public static String femaleNamesFile = "swiss_names_F.csv";
 	public static String lastnamesFile   = "swiss_lastnames.csv";
-	
-	public static String municipalitiesIndex = "municipalities";
-	public static String inhabitantsIndex    = "inhabitants";
-	
-	public static int totalInhabitants  = 8039060;
-	public static int transactionsSize  = 1000;
-	public static int maxMunicipalities = 9999;
 	
 	public static void main(String[] args) 
 	{		
@@ -35,15 +33,11 @@ public class Main
 			
 			if(removePrevious)
 				FileUtils.deleteDirectory(new File(dbPath));
-			
-			Creator creator = new Creator();
-			creator.openOrCreateDB();
-			creator.addGeographicalInfo();
-			creator.addInhabitants();
-			creator.addFriends();
-			creator.printStats();
-			creator.closeDB();
 						
+			Creator c = new Creator();
+			c.addGeographicalInfo();
+			c.addInhabitants();
+			
 			if(startStopService)
 				Runtime.getRuntime().exec(neoBin + " start-no-wait").waitFor();
 		} 
